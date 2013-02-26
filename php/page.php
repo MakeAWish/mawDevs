9a4523d528ede12a42032f2d05385cdd54133ba3
Boris Schapira <bschapira@clever-age.com>
2013-02-26 10:15:10 +0100

    DocType HTML5

--- php/page.php
+++ php/page.php
@@ -1,5 +1,5 @@
 <?php include Z::getFile('php_start'); ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<!doctype html>
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr" lang="fr" dir="ltr">
 <head>
 <?php include Z::getFile('head'); ?>
