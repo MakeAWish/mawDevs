5cd873245a1384b0096e236daf09d9f030230649
Boris Schapira <bschapira@clever-age.com>
2013-02-26 10:18:33 +0100

    validation w3c : for match id, not name

--- php/wish/content/login.php
+++ php/wish/content/login.php
@@ -5,8 +5,8 @@ if(isset($_GET['error'])) {
 ?>
 <form method="post">
 	<section class="typein">
-		<p class="typein"><label for="username">Login :</label><input type="text" placeholder=" Login" name="username"/></p>
-		<p class="typein"><label for="password">Mot de passe :</label><input type="password" placeholder=" Mot de passe"  name="password"/></p>
+		<p class="typein"><label for="username">Login :</label><input id="username" type="text" placeholder=" Login" name="username"/></p>
+		<p class="typein"><label for="password">Mot de passe :</label><input id="password" type="password" placeholder=" Mot de passe"  name="password"/></p>
 	</section>
 
 	<section class="submit_1">
