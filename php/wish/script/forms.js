3cb089dd339c2d800999b266c9ca09eb2fdaa93b
Boris Schapira <bschapira@clever-age.com>
2013-02-27 14:15:47 +0100

    Developpements pour le changement de Mdp

--- php/wish/script/forms.js
+++ php/wish/script/forms.js
@@ -1,13 +1,27 @@
-function formhash(form, password) {
+function formhash(form, password, new_password) {
    // Create a new element input, this will be out hashed password field.
    var p = document.createElement("input");
    // Add the new element to our form.
    form.appendChild(p);
    p.name = "p";
+
    p.type = "hidden"
    p.value = hex_sha512(password.value);
    // Make sure the plaintext password doesn't get sent.
    password.value = "";
+
+   if(new_password){
+      // Create a new element input, this will be out hashed password field.
+      var new_p = document.createElement("input");
+      // Add the new element to our form.
+      form.appendChild(new_p);
+      new_p.name = "new_p";
+
+      new_p.type = "hidden"
+      new_p.value = hex_sha512(new_password.value);
+      // Make sure the plaintext password doesn't get sent.
+      new_password.value = "";
+   }
    // Finally submit the form.
    form.submit();
 }
