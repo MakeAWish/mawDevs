6f14d8dc855053b211a7ae6a67852b3a42962c5e
Boris Schapira <bschapira@clever-age.com>
2013-04-12 15:09:23 +0200

    Ajout d'Intro.js, parametrage Wishlist pour l'exemple

--- php/wish/head/dist.php
+++ php/wish/head/dist.php
@@ -18,9 +18,15 @@ if (isset($_SESSION['user_id'])) {
 }
 ?>
 <link rel="stylesheet" href="style/<?php echo $styleSheet ?>.css"/>
+<!-- Add IntroJs styles -->
+<link href="style/introjs.css" rel="stylesheet">
+<!--[if lte IE 8]>
+<link href="style/introjs-ie.css" rel="stylesheet">
+<!-- <![endif]-->
 
 <script type="text/javascript" src="script/jquery-1.7.2.min.js"></script><!-- your jQuery version -->
 <script type="text/javascript" src="script/boutons_cadeau.js"></script>
 <script type="text/javascript" src="script/sha512.js"></script>
 <script type="text/javascript" src="script/forms.js"></script>
 <script type="text/javascript" src="script/modal.js"></script>
+<script type="text/javascript" src="script/intro.js"></script>
--- php/wish/header/dist.php
+++ php/wish/header/dist.php
@@ -4,6 +4,7 @@
         ?>
         <a href="?page=admin">Admin</a> |
     <?php } ?>
+    <span class="helpLink"><a href="javascript:void(0);" onclick="javascript:introJs().start();">Aide</a> |</span>
     <a href="?page=logout">Déconnexion</a>
 </div>
 
@@ -15,9 +16,9 @@
         $getUser->bindParam(':thisUser', $user_id);
         $getUser->execute();
         $user = $getUser->fetch(PDO::FETCH_OBJ);
-        if (!empty($user->surname)) {
-            echo ", <span>" . $user->surname . "</span>";
-        }
+        if (!empty($user->surname)) {?>
+            , <span data-step="1" data-intro="Vous êtes perdu ? Ne bougez pas, on va vous aider !"><?php echo $user->surname ?></span>
+        <?php }
         echo " *";
     } catch (PDOException $e) {
         echo 'Erreur : ' . $e->getMessage();
@@ -29,7 +30,14 @@ $menu = array(
     "wishlist" => "Ma Wishlist",
     "gift" => "Faire un cadeau",
     "giftlist" => "Ma Giftlist",
-    "profile" => "Mon Profil",
+    "profile" => "Mon Profil"
+);
+
+$help = array(
+    "wishlist" => "Ceci est votre Wishlist, vous pouvez y ajouter tous vos voeux",
+    "gift" => "D'humeur généreuse ? Vous êtes au bon endroit pour faire un cadeau",
+    "giftlist" => "Si on en croit nos registres, voici les cadeaux que vous devez offrir",
+    "profile" => "Ceci est votre page de profil"
 );
 
 $page = null;
@@ -44,6 +52,7 @@ if (isset($_GET['page']) AND $_GET['page'] == 'wishlist' AND isset($_GET['user']
             <li>
                 <a <?php if ($page == $key) {
                     echo 'class="active"';
+                    echo 'data-step="2" data-intro="'.$help[$key].'"';
                 }?> href="?page=<?php echo $key ?>">
                     <?php echo $value ?>
                 </a>
