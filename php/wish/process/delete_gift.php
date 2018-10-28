5945c05191d8f357970983318c10407412058830
Boris SCHAPIRA <borisschapira@gmail.com>
2018-10-28 12:28:27 +0100

    :bug: Fix gift deletion

--- php/wish/process/delete_gift.php
+++ php/wish/process/delete_gift.php
@@ -4,7 +4,7 @@ if (isset($_POST['data']) AND $_POST['data']) {
     $currentUserId = $_SESSION['user_id'];
 
     $query = $bdd->prepare("DELETE FROM gifts
-                                WHERE id IN ($giftsIds) AND iduser = :userid");
+                                WHERE idwish IN ($giftsIds) AND iduser = :userid");
     $query->bindParam(':userid', $currentUserId);
     $query->execute();
     if ($query->rowCount() > 0) {
